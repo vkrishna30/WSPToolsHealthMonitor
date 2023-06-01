844c47b12eee86c82c464e6f73d055ba24073982
vkrishna30 <47169125+vkrishna30@users.noreply.github.com>
2023-06-01 10:17:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [fetchData]);
 
   const fetchData = () => {
     fetch('https://wsptoolchecker.azurewebsites.net/api/data')
